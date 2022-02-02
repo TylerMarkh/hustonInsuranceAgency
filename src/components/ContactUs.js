7dc3eda4c00a53cb648d15545c14b280823543e9
Tyler Markham <tyler.markh@gmail.com>
2022-02-02 15:15:32 -0500

    Fixing form control for Netlify

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   Button,
@@ -12,6 +12,32 @@ import "@fontsource/libre-baskerville";
 import "@fontsource/source-sans-pro";
 
 const ContactUs = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitted");
+    setName("");
+    setPhone("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <Box m="2vw">
       <Text
@@ -26,43 +52,68 @@ const ContactUs = () => {
         Contact us using this form below.
       </Text>
       <Box width="100%" display="flex" justifyContent="center" pt="2vh">
-        <FormControl
-          name="contact"
+        <form
           method="post"
           dataNetlify="true"
-          isRequired
-          boxShadow="0 4px 6px 0 hsla(0, 0%, 0%, 0.2)"
-          width={{ base: "90vw", md: "60vw", lg: "30vw" }}
-          border="3px solid rgba(43, 59, 43, 0.6)"
-          p="2vh"
-          borderRadius="2vh"
-          mb="4vh"
+          name="contact"
+          onSubmit={handleSubmit}
         >
-          <Input type="hidden" name="contract" value="contact" />
-          <FormLabel htmlFor="name" pt="1vh" borderRadius="2vh">
-            Name
-          </FormLabel>
-          <Input id="name" />
-          <FormLabel id="phone-number" htmlFor="number">Phone number</FormLabel>
-          <Input id="phone-number" type="number" />
-          <FormLabel htmlFor="email" pt="1vh">
-            Email address
-          </FormLabel>
-          <Input id="email" type="email" />
-          <FormLabel htmlFor="message" pt="1vh">
-            Message
-          </FormLabel>
-          <Textarea id="message" mb="1vh"></Textarea>
-          <Button
-            type="submit"
-            mt="2vh"
-            bgColor="rgba(43, 59, 43)"
-            color="white"
-            id="submit"
+          <FormControl
+            method="post"
+            dataNetlify="true"
+            name="contact"
+            isRequired
+            boxShadow="0 4px 6px 0 hsla(0, 0%, 0%, 0.2)"
+            width={{ base: "90vw", md: "60vw", lg: "30vw" }}
+            border="3px solid rgba(43, 59, 43, 0.6)"
+            p="2vh"
+            borderRadius="2vh"
+            mb="4vh"
           >
-            Submit
-          </Button>
-        </FormControl>
+            <Input type="hidden" name="contract" value="contact" />
+            <FormLabel htmlFor="name" pt="1vh" borderRadius="2vh">
+              Name
+            </FormLabel>
+            <Input id="name" value={name} onChange={handleNameChange} />
+            <FormLabel id="phone-number" htmlFor="number">
+              Phone number
+            </FormLabel>
+            <Input
+              id="phone-number"
+              type="number"
+              value={phone}
+              onChange={handlePhoneChange}
+            />
+            <FormLabel htmlFor="email" pt="1vh">
+              Email address
+            </FormLabel>
+            <Input
+              id="email"
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <FormLabel htmlFor="message" pt="1vh">
+              Message
+            </FormLabel>
+            <Textarea
+              id="message"
+              mb="1vh"
+              value={message}
+              onChange={handleMessageChange}
+            ></Textarea>
+            <Button
+              type="submit"
+              onSubmit={handleSubmit}
+              mt="2vh"
+              bgColor="rgba(43, 59, 43)"
+              color="white"
+              id="submit"
+            >
+              Submit
+            </Button>
+          </FormControl>
+        </form>
       </Box>
     </Box>
   );
